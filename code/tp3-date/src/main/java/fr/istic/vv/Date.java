6e4dc189b0df7ce06e46585532c3a7af6c9fdc09
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2022-12-03 18:23:35 +0100

    add date class

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,89 @@
 package fr.istic.vv;
 
+import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.TreeMap;
+
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    int day;
+    int month;
+    int year;
+    static Map<Integer, Integer> joursMois;
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public Date(int day, int month, int year) throws IllegalArgumentException {
 
-    public static boolean isLeapYear(int year) { return false; }
+        if (isValidDate(day, month, year)) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+            joursMois = new TreeMap<Integer, Integer>();
+            joursMois.put(1, 31);
+            joursMois.put(2, 28);
+            joursMois.put(3, 31);
+            joursMois.put(4, 30);
+            joursMois.put(5, 31);
+            joursMois.put(6, 30);
+            joursMois.put(7, 31);
+            joursMois.put(8, 31);
+            joursMois.put(9, 30);
+            joursMois.put(10, 31);
+            joursMois.put(11, 30);
+            joursMois.put(12, 31);
+        } else
+            throw new IllegalArgumentException();
+    }
 
-    public Date nextDate() { return null; }
+    public static boolean isValidDate(int day, int month, int year) {
+        if (day > 0 && month > 0 && month <= 12) {
+            if ((isLeapYear(year) && month == 2 && day <= 29) || joursMois.get(month) >= day) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    public Date previousDate() { return null; }
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public Date nextDate() {
+        if (!isValidDate(day + 1, month, year)) {
+            return new Date(day + 1, month, year);
+        } else if (!isValidDate(1, month + 1, year)) {
+            return new Date(1, month + 1, year);
+        } else {
+            return new Date(1, 1, year + 1);
+        }
+    }
 
+    public Date previousDate() {
+        if (!isValidDate(day - 1, month, year)) {
+            return new Date(day - 1, month, year);
+        } else if (!isValidDate(joursMois.get(month - 1), month - 1, year)) {
+            return new Date(joursMois.get(month - 1), month - 1, year);
+        } else {
+            return new Date(joursMois.get(12), 12, year - 1);
+        }
+    }
+
+    public int compareTo(Date other) {
+        if (other != null) {
+            if (other.year < this.year) {
+                return 1;
+            } else if (other.year > this.year) {
+                return -1;
+            } else if (other.month < this.month) {
+                return 1;
+            } else if (other.month > this.month) {
+                return -1;
+            } else if (other.day < this.day) {
+                return 1;
+            } else if (other.day > this.day) {
+                return -1;
+            } else
+                return 0;
+        } else
+            throw new NullPointerException();
+    }
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -4,7 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class DateTest {
 
 
+    List<Date> testSet = new ArrayList<Date>();
+
+    private void fillTestSet(int[][] dates) {
+        for (int[] date : dates) {
+            testSet.add(new Date(date[0], date[1], date[2]));
+        }
+    }
+
+    private void clearTestSet() {
+        testSet.clear();
+    }
+
+
+    @Test
+    void testDate() {
+        int[][] testSet = {{1,1,2000}, {-20,5,1990}, {31,4,1500}};
+    }
+
+
+    @Test
+    void testIsValidDate() {
+        int[][] testSet = {{1,1,2000}, {-20,5,1990}, {31,4,1500}};
+
+    }
 }
