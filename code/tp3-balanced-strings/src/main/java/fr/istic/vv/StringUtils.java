8b60c6bdb67238fbf27d056be500089d7d5d0d9b
marouane <marouane.ri@gmail.cm>
2022-12-08 22:55:17 +0100

    last commit

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,41 @@
 package fr.istic.vv;
 
+import java.util.Stack;
+
 public class StringUtils {
 
-    private StringUtils() {}
-
     public static boolean isBalanced(String str) {
+      try {
+        Stack<Character> st = new Stack<>();
+      
+        //parcours la chaine de caractère
+        for(char ch : str.toCharArray()) {
+          if(ch == '{' || ch == '(' || ch == '[') {
+            //push the character if it's { or [ or (
+            st.push(ch);
+          } else {
+            if(st.empty()) {
+              return false;
+            }
+            
+            char dernierParOuverant = st.pop();
+            
+            if(dernierParOuverant == '{' && ch != '}') {
+              return false;
+            } else if (dernierParOuverant == '(' && ch != ')') { // this for parentheses
+              return false;
+            } else if (dernierParOuverant == '[' && ch != ']') { // for square braces
+              return false;
+            }
+          }
+          
+        }
+        return st.size() == 0;
+  
+      }catch(Exception e){
+        System.out.println("The value passe in parameter is probably null!");
         return false;
+      }    
     }
 
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+  @Test
+  public void testIsBalancedWithBalancedPars() throws Exception {
+    assertTrue(StringUtils.isBalanced("{[][]}({})"));
+  }
+
+  @Test
+  public void testIsBalancedUnbalancedPars() throws Exception {
+    assertFalse(StringUtils.isBalanced("{][}{})"));
+  }
+
+  @Test
+  public void testIsBalancedEmtyText() throws Exception {
+    assertTrue(StringUtils.isBalanced(""));
+  }
+
+  @Test
+  public void testIsBalancedNullParameter() throws Exception {
+    assertFalse(StringUtils.isBalanced(null));
+  }
 
 }
